d39859f2141ed366c34fe84c0c0f8c2a92d718e1
Joy Debnath <69497013+Debnath-J@users.noreply.github.com>
2020-08-13 14:20:55 +0600

    Basic DNA.py
    Python 3.x program to get basic information from DNA like length of a DNA sequence, number of A/T/C/G, number of purine and pyrimidine bases, gc content,unidentified bases, reverse complementary sequences.

--- Basic_DNA.py
+++ Basic_DNA.py
@@ -3,21 +3,21 @@
 length of a DNA sequence, number of A/T/C/G, number of purine and pyrimidine bases,
 gc content,unidentified bases, reverse complementary sequences."""
 
-dna=input("Enter your DNA seqeuence here:").lower() #take DNA seq. from user
+dna=input("Enter your DNA seqeuence here:").lower()                            #take DNA seq. from user
 
-print("Length of your given DNA seqeucne is", len(dna)) #to get the length
-print("Your DNA sequence contains:", dna.count('a'),"Adenine; ", dna.count('c'),"Cytosine; ",dna.count('g'),"Guanine; ",dna.count('t'),"Thymine.") #to get the number of bases
-print("Purine:", dna.count('a')+dna.count('g')) #number of purine bases
-print("Pyrimidine:", dna.count('c')+dna.count('t')) #number of pyrimidine bases
-print("GC content:", (dna.count('c')+dna.count('g'))*100/len(dna)) #gc content
-print("Unidentified bases:", dna.count('n')) #unidentified bases (n)
+print("Length of your given DNA seqeucne is", len(dna))                        #to get the length
+print("Your DNA sequence contains:", dna.count('a'),"Adenine; ", dna.count('c'),"Cytosine; ",dna.count('g'),"Guanine; ",dna.count('t'),"Thymine.")           #to get the number of bases
+print("Purine:", dna.count('a')+dna.count('g'))                                #number of purine bases
+print("Pyrimidine:", dna.count('c')+dna.count('t'))                            #number of pyrimidine bases
+print("GC content:", (dna.count('c')+dna.count('g'))*100/len(dna))             #gc content
+print("Unidentified bases:", dna.count('n'))                                   #unidentified bases (n)
 
 #Reverse Complementary Sequences
 
-dna2=dna[::-1] #to reverse the dna
-basecomplement={'a':'t','c':'g','g':'c','t':'a'} #complementary bases for bases in leading strand
-letters=list(dna2) #make a list from the DNA string
-dna2=[basecomplement[base] for base in letters] #To substitue the bases of DNA with complementary bases.
-dna2=''.join(dna2) #make it a string again.
+dna2=dna[::-1]                                                                 #to reverse the dna
+basecomplement={'a':'t','c':'g','g':'c','t':'a'}                               #complementary bases for bases in leading strand
+letters=list(dna2)                                                             #make a list from the DNA string
+dna2=[basecomplement[base] for base in letters]                                #To substitue the bases of the DNA with complementary bases.
+dna2=''.join(dna2)                                                             #make it a string again, and "dna2" will be the reverse complementary seq. of the given DNA. 
 print("Reverse transcription of your given DNA seqeuence:")
-print(dna2) #to show the result
+print(dna2)                                                                    #to show the result
